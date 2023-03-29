d52cdf9f9699af172bfda14ef72610349fd6f3fe
Knoxvillys <knoxville42@gmail.com>
2023-03-29 23:52:55 +0700

    Задание 2 + дополнительные

--- geodata/models.py
+++ geodata/models.py
@@ -1,14 +1,13 @@
 from django.contrib.gis.db import models
 
 
-
-
 class LineString(models.Model):
     name = models.CharField(max_length=250, verbose_name='Имя.')
     geometry = models.LineStringField(verbose_name='Поле геометрии. ')
 
     def __str__(self):
         return self.name
+
 
 class Point(models.Model):
     name = models.CharField(max_length=250, verbose_name='Имя')
