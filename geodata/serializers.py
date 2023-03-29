f5f7836523e5aa69258842bf06f8fbbc0f3e2c09
Knoxvillys <knoxville42@gmail.com>
2023-03-29 23:30:32 +0700

    Задание 2 + дополнительные

--- geodata/serializers.py
+++ geodata/serializers.py
@@ -1,22 +1,37 @@
-from rest_framework import serializers
+from rest_framework import serializers, SlugRelatedField
+from rest_framework_gis.serializers import GeoFeatureModelSerializer # сериализатор для GeoJISON
 
 from .models import Polygon, LineString, Point
 
 
-class PolygonSerializer(serializers.ModelSerializer):
+class PolygonSerializer(GeoFeatureModelSerializer):
+    points = SlugRelatedField(
+        queryset=Point.objects.all(),
+        allow_null=True,
+        required=False,
+        slug_field='name',
+        many=True
+    )
     
     class Meta:
         model = Polygon
-        fields = '__all__'
+        geo_field = "geometry" # требуется для определения GeoFeatureModelSerializer
+        id_field = False # Первичный ключ модели (обычно атрибут «id») автоматически используется в качестве id поля каждого объекта GeoJSON Feature Object .
+        fields = ['geometry', 'id', 'name', 'points']
 
 
-class LineStringSerializer(serializers.ModelSerializer):
+class LineStringSerializer(GeoFeatureModelSerializer):
+
     class Meta:
         model = LineString
-        fields = '__all__'
+        geo_field = "geometry"
+        id_field = False
+        fields = ['geometry', 'id', 'name']
 
 
-class PointSerializer(serializers.ModelSerializer):
+class PointSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = Point
-        fields = '__all__'
+        geo_field = 'geometry'
+        id_field = False
+        fields = ['geometry', 'id', 'name']
--- geodata/views.py
+++ geodata/views.py
@@ -1,29 +1,50 @@
-from django.shortcuts import render
 from rest_framework.generics import RetrieveUpdateAPIView, ListCreateAPIView, RetrieveDestroyAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
+from rest_framework_gis.filters import DistanceToPointFilter # Фильтр расстояния до точки
+
 
 from .models import Polygon, LineString, Point
 from .serializers import PolygonSerializer, LineStringSerializer, PointSerializer
+from .Mixin import CreateListModelMixin
 
 
-class PolygonView(ListCreateAPIView):
-    '''
-    Создание и получение данных из таблицы "Polygon"
-    '''
+
+class PolygonView(viewsets.ModelViewSet):
+    """
+
+    """
     queryset = Polygon.objects.all()
     serializer_class = PolygonSerializer
+    
+
+    filter_backends = [DistanceToPointFilter, DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['name']
+    search_fields = ['name']
+
+    distance_filter_field = 'geometry'
+    distance_filter_convert_meters = True #Для того чтобы преобразовать входное расстояние из метров в градусы
 
 
 class LineStringView(ListCreateAPIView):
-    '''
-    Создание и получение данных из таблицы "LineString"
-    '''
+    """
+
+    """
     queryset = LineString.objects.all()
-    serializer_class = PolygonSerializer
+    serializer_class = LineStringSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['name']
+    search_fields = ['name']
 
 
-class PointView(ListCreateAPIView):
-    '''
-    Создание и получение данных из таблицы "Point"
-    '''
+class PointView(CreateListModelMixin, viewsets.ModelViewSet):
+    """
+
+    """
     queryset = Point.objects.all()
-    serializer_class = PolygonSerializer
+    serializer_class = PointSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['name']
+    search_fields = ['name']
